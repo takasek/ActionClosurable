3b3e560da0f2a473ee895081b2710e61a143d161
Yoshitaka Seki <takassekiyoshi@gmail.com>
2016-04-16 18:03:28 +0900

    Separate functions

--- ActionClosurable/ActionClosurable.swift
+++ ActionClosurable/ActionClosurable.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 public class Actor<T> {
     @objc func act(sender: AnyObject) { closure(sender as! T) }
@@ -21,23 +20,21 @@ private class GreenRoom {
     private var actors: [Any] = []
 }
 private var GreenRoomKey: UInt32 = 893
-private func register<T>(closure: T -> Void, to object: AnyObject, @noescape configure: (Actor<T>, Selector) -> Void) {
-    let actor = Actor(closure)
-
+private func register<T>(actor: Actor<T>, to object: AnyObject) {
     let room = objc_getAssociatedObject(object, &GreenRoomKey) as? GreenRoom ?? {
         let room = GreenRoom()
         objc_setAssociatedObject(object, &GreenRoomKey, room, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         return room
         }()
     room.actors.append(actor)
-
-    configure(actor, #selector(Actor<T>.act(_:)))
 }
 
 public protocol ActionClosurable {}
 extension ActionClosurable where Self: AnyObject {
     func registerClosure<T>(closure: T -> Void, @noescape configure: (Actor<T>, Selector) -> Void) {
-        register(closure, to: self, configure: configure)
+        let actor = Actor(closure)
+        register(actor, to: self)
+        configure(actor, #selector(Actor<T>.act(_:)))
     }
 }
 
