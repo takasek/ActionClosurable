d3ff04ba4673383a4ff14acfc31846e7e1e73cdc
Yoshitaka Seki <takassekiyoshi@gmail.com>
2016-04-16 19:48:08 +0900

    Add framework Target

---
+++ ActionClosurable/ActionClosurable.h
@@ -0,0 +1,19 @@
+//
+//  ActionClosurable.h
+//  ActionClosurable
+//
+//  Created by Yoshitaka Seki on 2016/04/16.
+//  Copyright © 2016年 Yoshitaka Seki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActionClosurable.
+FOUNDATION_EXPORT double ActionClosurableVersionNumber;
+
+//! Project version string for ActionClosurable.
+FOUNDATION_EXPORT const unsigned char ActionClosurableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionClosurable/PublicHeader.h>
+
+
