ed3b72569a7e19cf434fe95cb4e21dfb6a630dbd
Yoshitaka Seki <takassekiyoshi@gmail.com>
2016-09-22 20:00:41 +0900

    Make Actor private

--- ActionClosurable/ActionClosurable.swift
+++ ActionClosurable/ActionClosurable.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public class Actor<T> {
+private class Actor<T> {
     @objc func act(sender: AnyObject) { closure(sender as! T) }
     fileprivate let closure: (T) -> Void
     init(acts closure: @escaping (T) -> Void) {
@@ -32,12 +32,12 @@ private func register<T>(_ actor: Actor<T>, to object: AnyObject) {
 
 public protocol ActionClosurable {}
 public extension ActionClosurable where Self: AnyObject {
-    public func convert(closure: @escaping (Self) -> Void, toConfiguration configure: (Actor<Self>, Selector) -> Void) {
+    public func convert(closure: @escaping (Self) -> Void, toConfiguration configure: (AnyObject, Selector) -> Void) {
         let actor = Actor(acts: closure)
         configure(actor, #selector(Actor<AnyObject>.act(sender:)))
         register(actor, to: self)
     }
-    public static func convert(closure: @escaping (Self) -> Void, toConfiguration configure: (Actor<Self>, Selector) -> Self) -> Self {
+    public static func convert(closure: @escaping (Self) -> Void, toConfiguration configure: (AnyObject, Selector) -> Self) -> Self {
         let actor = Actor(acts: closure)
         let instance = configure(actor, #selector(Actor<AnyObject>.act(sender:)))
         register(actor, to: instance)
